bfcd0395e23ee484495b17a81837d48a5d8ab54f
chienleow <chienleow1@gmail.com>
2021-03-30 21:49:01 -0400

    add validation in cookie model

--- app/models/cookie.rb
+++ app/models/cookie.rb
@@ -2,7 +2,7 @@ class Cookie < ActiveRecord::Base
   belongs_to :storage, polymorphic: :true
   
   validates :storage, presence: true
-  validates :fillings, presence: true
+  validates_presence_of :fillings, message: 'no fillings'
 
   def ready?
     true
